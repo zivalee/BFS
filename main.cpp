38bcec9ed742b6fdf601da3a65137a23641f54d2
Ziva <zivalee87@gmail.com>
2020-12-29 13:30:04 +0800

    rearrange code

--- main.cpp
+++ main.cpp
@@ -7,108 +7,6 @@ using namespace std;
 // 1. BFS 長樹
 // 2. SRPT 估 LB
 
-int estimateLB(vector<pair<int, int>> proc, int n);
-
-int calScheduledSumC(vector<pair<int, int>> node);
-
-int main() {
-    // x: burst time, y: arrival time
-    vector<pair<int, int>> jobs = {{6, 0},
-                                   {2, 2},
-                                   {3, 2},
-                                   {2, 6},
-                                   {5, 7},
-                                   {2, 9}};
-    int n = 6;
-    // create min heap according to x(objective value)
-    typedef pair<int, vector<int>> permutation;
-    priority_queue<permutation, vector<permutation>, greater<permutation>> heap;
-
-    // {pair{process time, permutation{}}}
-    for (int i = 0; i < n; i++) {
-        // i = scheduled job
-        int sumC = jobs[i].first + jobs[i].second;
-        // estimate not scheduled jobs (use SRPT)
-        vector<pair<int, int>> originJobs = jobs;
-        originJobs.erase(originJobs.begin() + i);
-        int estimate = estimateLB(originJobs, n - 1);
-
-        heap.push(make_pair(sumC + estimate, vector<int>{i + 1}));
-    }
-
-
-    while (heap.top().second.size() != n) {
-        vector<int> origin = heap.top().second;
-        heap.pop();
-
-        for (int i = 1; i <= n; i++) {
-            vector<int> vec = origin;
-
-            // if job is not scheduled yet
-            if (find(vec.begin(), vec.end(), i) == vec.end()) {
-                vec.push_back(i);
-
-                // updated scheduled jobs;
-                vector<int> scheduled;  // job index
-                vector<pair<int, int>> toCount;
-                // updated unscheduled jobs;
-                vector<pair<int, int>> unscheduledToCount;
-                for (int j = 0; j < vec.size(); j++) {
-                    toCount.push_back(jobs.at(vec.at(j) - 1));
-                    scheduled.push_back(vec.at(j));
-                }
-
-                for (int a = 1; a <= n; a++) {
-                    if (find(scheduled.begin(), scheduled.end(), a) == scheduled.end()) {
-                        unscheduledToCount.push_back(jobs.at(a - 1));
-                    }
-                }
-                // print scheduled jobs index
-                for (int x = 0; x < vec.size(); x++) {
-                    cout << vec[x];
-                }
-                cout << endl;
-
-                // print scheduled jobs
-//                for (int x = 0; x < toCount.size(); x++) {
-//                    cout << toCount.at(x).first;
-//                    cout << toCount.at(x).second << endl;
-//                }
-//                cout << endl;
-
-                // print unscheduledToCount
-//                for (int x = 0; x < unscheduledToCount.size(); x++) {
-//                    cout << unscheduledToCount.at(x).first << unscheduledToCount.at(x).second << endl;
-//                }
-//                cout << "------" << endl;
-
-
-                int scheduledSumC = calScheduledSumC(toCount);
-                int unscheduledSumC = 0;
-                if (unscheduledToCount.size() != 0) {
-                    unscheduledSumC = estimateLB(unscheduledToCount, unscheduledToCount.size());
-                }
-
-                heap.push(make_pair(scheduledSumC + unscheduledSumC, vec));
-
-//                cout << "scheduled:" << scheduledSumC << endl;
-//                cout << "unscheduled:" << unscheduledSumC << endl;
-//                cout << "-------------------------" << endl;
-
-            }
-        }
-    }
-
-    cout <<"Best: "<< heap.top().first << endl;
-    cout << "Best Permutation: ";
-    for (int x = 0; x < heap.top().second.size(); x++) {
-        cout << heap.top().second[x];
-    }
-
-    return 0;
-}
-
-
 int calScheduledSumC(vector<pair<int, int>> node) {
     vector<int> sum;
     auto sumC = 0;
@@ -213,4 +111,103 @@ int estimateLB(vector<pair<int, int>> proc, int n) {
     }
     delete heap;
     return total;
+}
+
+int main() {
+    // x: burst time, y: arrival time
+    vector<pair<int, int>> jobs = {{6, 0},
+                                   {2, 2},
+                                   {3, 2},
+                                   {2, 6},
+                                   {5, 7},
+                                   {2, 9}};
+    int n = 6;
+    // create min heap according to x(objective value)
+    typedef pair<int, vector<int>> permutation;
+    priority_queue<permutation, vector<permutation>, greater<permutation>> heap;
+
+    // {pair{process time, permutation{}}}
+    // initial jobs
+    for (int i = 0; i < n; i++) {
+        // i = scheduled job
+        int sumC = jobs[i].first + jobs[i].second;
+        // estimate not scheduled jobs (use SRPT)
+        vector<pair<int, int>> originJobs = jobs;
+        originJobs.erase(originJobs.begin() + i);
+        int estimate = estimateLB(originJobs, n - 1);
+
+        heap.push(make_pair(sumC + estimate, vector<int>{i + 1}));
+    }
+
+
+    while (heap.top().second.size() != n) {
+        vector<int> origin = heap.top().second;
+        heap.pop();
+
+        for (int i = 1; i <= n; i++) {
+            vector<int> vec = origin;
+
+            // if job is not scheduled yet
+            if (find(vec.begin(), vec.end(), i) == vec.end()) {
+                vec.push_back(i);
+
+                // updated scheduled jobs;
+                vector<int> scheduled;  // job index
+                vector<pair<int, int>> toCount;
+                // updated unscheduled jobs;
+                vector<pair<int, int>> unscheduledToCount;
+                for (int j = 0; j < vec.size(); j++) {
+                    toCount.push_back(jobs.at(vec.at(j) - 1));
+                    scheduled.push_back(vec.at(j));
+                }
+
+                for (int a = 1; a <= n; a++) {
+                    if (find(scheduled.begin(), scheduled.end(), a) == scheduled.end()) {
+                        unscheduledToCount.push_back(jobs.at(a - 1));
+                    }
+                }
+                // print scheduled jobs index
+                for (int x = 0; x < vec.size(); x++) {
+                    cout << vec[x];
+                }
+                cout << endl;
+
+                // print scheduled jobs
+//                for (int x = 0; x < toCount.size(); x++) {
+//                    cout << toCount.at(x).first;
+//                    cout << toCount.at(x).second << endl;
+//                }
+//                cout << endl;
+
+                // print unscheduledToCount
+//                for (int x = 0; x < unscheduledToCount.size(); x++) {
+//                    cout << unscheduledToCount.at(x).first << unscheduledToCount.at(x).second << endl;
+//                }
+//                cout << "------" << endl;
+
+
+                int scheduledSumC = calScheduledSumC(toCount);
+                int unscheduledSumC = 0;
+                if (unscheduledToCount.size() != 0) {
+                    unscheduledSumC = estimateLB(unscheduledToCount, unscheduledToCount.size());
+                }
+
+                heap.push(make_pair(scheduledSumC + unscheduledSumC, vec));
+
+//                cout << "scheduled:" << scheduledSumC << endl;
+//                cout << "unscheduled:" << unscheduledSumC << endl;
+//                cout << "-------------------------" << endl;
+
+            }
+        }
+        cout << "-----" << endl;
+    }
+
+    cout <<"Best: "<< heap.top().first << endl;
+    cout << "Best Permutation: ";
+    for (int x = 0; x < heap.top().second.size(); x++) {
+        cout << heap.top().second[x];
+    }
+
+    return 0;
 }
